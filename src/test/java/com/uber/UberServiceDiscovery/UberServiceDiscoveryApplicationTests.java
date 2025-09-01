31fc01f6339b54e90fe89fc1320cd85917e75e71
Ayush Thakur <ayush26819@gmail.com>
2025-09-01 22:10:38 +0530

    Setup eureka server

---
+++ src/test/java/com/uber/UberServiceDiscovery/UberServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.UberServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
